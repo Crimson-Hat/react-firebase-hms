cd22ba93514feef4cb204035a25857baf504668d
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-09-22 07:20:57 -0400

    Create setpersondetailsaction.js

---
+++ src/Images/setpersondetailsaction.js
@@ -0,0 +1,18 @@
+export const setpersonDetails = (p) => {
+  return {
+    type: "SET_PERSONDETAILS_DETAILS",
+    payload: p,
+  };
+};
+export const setReportDetails = (p) => {
+  return {
+    type: "SET_REPORT_DETAILS",
+    payload: p,
+  };
+};
+export const setLogInDetails = (p) => {
+  return {
+    type: "SET_LOGIN_DETAILS",
+    payload: p,
+  };
+};
